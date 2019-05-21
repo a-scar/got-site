06a8c7c69b58c9236be6798aa75bc0affabffd2a
Anthony Scardapane <anthonyscardapane@Anthonys-MacBook-Pro.local>
2019-05-21 13:13:02 -0400

    another commit

--- src/app/players.service.ts
+++ src/app/players.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Player } from './leader-board/player';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({
